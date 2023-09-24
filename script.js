068b9c219aa784ba25c5caf28e7ac2841b54e4f6
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-24 14:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+	 function onDOMLoad() {
+            var textNode = document.createTextNode("DOM load success");
+            var body = document.body;
+            while (body.firstChild) {
+                body.removeChild(body.firstChild);
+            }
+            body.appendChild(textNode);
+        }
+        document.addEventListener("DOMContentLoaded", onDOMLoad);
