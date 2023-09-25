c29ed4f811554879fde446ddb223e2abcf4955ab
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-25 14:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //your JS code here. If required.
-	 function onDOMLoad() {
-		 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-});
+	 function domloaded() {
+		 const messageDiv = document.getElementById("message");
+
+            // Create a text node containing the message
+            const textNode = document.createTextNode("DOM load success");
+
+            // Append the text node to the message div
+            messageDiv.appendChild(textNode);
 
           
-        }
+    }
+   document.addEventListener("DOMContentLoaded", domLoaded);
       
